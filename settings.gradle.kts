30ce1b15031374f18da153d1b14aa163265357b4
GitHub Action <action@github.com>
2022-09-20 04:46:42 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "friendly-octo-waddle"
--- src/main/kotlin/com/github/undeadbanana/friendlyoctowaddle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/undeadbanana/friendlyoctowaddle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.undeadbanana.friendlyoctowaddle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.undeadbanana.friendlyoctowaddle.MyBundle
 
 class MyApplicationService {
 
